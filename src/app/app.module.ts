9a0b44bf2d45671aaace5f8fbf2f5d3be9def16f
alfaiz-momin <alfaizmomin@gmail.com>
2019-07-26 19:50:47 +0530

    category done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SliderCategoryComponent } from './slider-category/slider-category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderCategoryComponent
   ],
   imports: [
     BrowserModule,
